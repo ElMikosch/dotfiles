5d36bbf8517816514f24b0afc0beaa370c981b13
Mathias Mikosch <mmikosch@dspace.de>
2025-01-07 09:56:12 +0100

    added tmux config

--- dot_config/nvim/lua/plugins/undotree.lua
+++ dot_config/nvim/lua/plugins/undotree.lua
@@ -3,7 +3,7 @@ return {
     "mbbill/undotree",
     opts = {},
     config = function()
-      vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+      vim.keymap.set("n", "<leader>U", vim.cmd.UndotreeToggle)
     end,
   },
 }
